d39122009f32eff8c20c343b2c074807d3d71de5
puran <puran@DESKTOP-G15S51P>
2020-04-25 17:50:13 +0900

    5766

---
+++ 5766.GrandpaIsFamous/src/Bridge.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Bridge {
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int m, n;
+		
+		while(true){
+			m = s.nextInt();
+			n = s.nextInt();
+			if(m == 0)
+				break;
+			else {
+				int[] player = new int[10000];
+				for(int i = 0; i<10000; i++)
+					player[i] = 0;
+			
+				int k;
+				for(int i = 0; i<m*n; i++) {
+					k = s.nextInt();
+					player[k-1] = player[k-1] + 1;		
+				}
+				int a = 1;
+				int b = 0;
+				for(int i = 0; i<10000; i++) {
+					if(player[i]>a) {
+						b = a;
+						a = player[i];
+					}
+					else if(a<player[i] && player[i]<b)
+						b = player[i];
+				}
+				for(int i = 0; i<10000; i++) {
+					if(player[i] == b)
+						System.out.println(i+1);
+					}		
+			}		
+		}
+	}
+}
