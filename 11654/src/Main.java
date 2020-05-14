bdb816925cfac937a9fc912b1195c415d17b2ab8
puran <puran@DESKTOP-G15S51P>
2020-05-14 09:03:29 +0900

    ps

---
+++ 11654/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String str = s.next();
+		char cha = str.charAt(0);
+		int askii = cha;
+		
+		System.out.println(askii);
+		
+	}
+
+}
